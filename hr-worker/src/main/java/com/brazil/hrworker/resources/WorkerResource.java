ada3ea5fa5236b2b951d804f64527ebe73e85df2
eduardomingoranca <eduardomingoranca@gmail.com>
2021-08-21 15:04:49 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/brazil/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/brazil/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.brazil.hrworker.services.WorkerService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,8 +23,19 @@ public class WorkerResource {
 
     private static Logger LOGGER = LoggerFactory.getLogger(WorkerResource.class);
 
+    @Value("${test.config}")
+    private String testConfig;
+
     private final WorkerService workerService;
     private final Environment environment;
+
+    /**
+     * recebendo as configurações */
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        LOGGER.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
 
     @GetMapping
     public ResponseEntity<List<WorkerResponse>> list() {
@@ -33,14 +45,16 @@ public class WorkerResource {
     @GetMapping(path = "/{id}")
     public ResponseEntity<WorkerResponse> findById(@PathVariable long id) {
 
-        /*try {
+        try {
             Thread.sleep(300L);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
+        }
 
         LOGGER.info("PORT = " + environment.getProperty("local.server.port"));
         return new ResponseEntity(workerService.findByIdOrThrowBadRequestException(id), HttpStatus.OK);
     }
+
+
 
 }
