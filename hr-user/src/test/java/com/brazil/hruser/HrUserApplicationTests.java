d5e1c0e76a23d3c4a14fd84ffa5229cc0c8eed16
eduardomingoranca <eduardomingoranca@gmail.com>
2021-08-22 16:04:25 -0300

    project hr-user created

---
+++ hr-user/src/test/java/com/brazil/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brazil.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
