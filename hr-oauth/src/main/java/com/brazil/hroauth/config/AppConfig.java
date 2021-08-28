5c8ae8d599277bd4b682efab1284c127b73ee463
eduardomingoranca <eduardomingoranca@gmail.com>
2021-08-28 08:30:08 -0300

    configuring enable authorization server

--- hr-oauth/src/main/java/com/brazil/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/brazil/hroauth/config/AppConfig.java
@@ -1,4 +1,34 @@
 package com.brazil.hroauth.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
 public class AppConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    /**
+     * convertendo o acesso em Token.*/
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+        /**
+         * configurando a chave secreta que será assinada os tokens */
+        tokenConverter.setSigningKey("MY-SECRET-KEY");
+        return tokenConverter;
+    }
+
+    /**
+     * objeto responsavel por ler as informações do token */
+    @Bean
+    public JwtTokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
 }
