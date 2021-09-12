29cf7675651f5dbaa16df3fc01c02c66e4779307
eduardomingoranca <eduardomingoranca@gmail.com>
2021-09-12 15:24:38 -0300

    hr-api-gateway-zuul container

--- hr-api-gateway-zuul/src/main/java/com/brazil/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/brazil/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,6 +1,7 @@
 package com.brazil.hrapigatewayzuul.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
